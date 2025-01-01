305ca692fba099e8c73d37e097892a1171ec0166
Yogesh Patidar <yogeshpatidar340@gmail.com>
2025-01-01 13:13:15 +0530

    captain authenticated done

--- BACKEND/controllers/captain.controller.js
+++ BACKEND/controllers/captain.controller.js
@@ -38,7 +38,7 @@ module.exports.registerCaptain = async (req, res, next) => {
     res.status(201).json({ token, captain });
 
 }
-
+// login captain 
 module.exports.loginCaptain = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -65,15 +65,17 @@ module.exports.loginCaptain = async (req, res, next) => {
 
     res.status(200).json({ token, captain });
 }
-
+// get captain profile 
 module.exports.getCaptainProfile = async (req, res, next) => {
     res.status(200).json({ captain: req.captain });
 }
-
+// logout captain 
 module.exports.logoutCaptain = async (req, res, next) => {
     const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
 
     await blackListTokenModel.create({ token });
+    console.log(blackListTokenModel);
+
 
     res.clearCookie('token');
 
