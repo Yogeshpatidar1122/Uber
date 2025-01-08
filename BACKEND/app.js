9d2b7b1358def311c1139ff69fce31c1dbc6f244
Yogesh Patidar <yogeshpatidar340@gmail.com>
2025-01-08 13:39:29 +0530

    backend done

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -7,6 +7,8 @@ const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 const cookieParser = require('cookie-parser');
 const captainRoutes = require('./routes/captain.routes');
+const mapRoutes = require('./routes/map.routes');
+const rideRoutes = require('./routes/ride.routes');
 connectToDb();
 
 app.use(cors());
@@ -18,6 +20,8 @@ app.get('/', (req, res) => {
     res.send('hello world');
 });
 app.use('/user', userRoutes);
-app.use('/captain', captainRoutes)
+app.use('/captain', captainRoutes);
+app.use('/maps', mapRoutes);
+app.use('/rides', rideRoutes);
 
 module.exports = app;
